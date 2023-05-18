c9e286b10fa690f7b7b12e44c1efbe38c74381ef
Alex Durrant <alexjdurrant@gmail.com>
2023-05-18 08:15:42 -0600

    Fixed the spelling error

--- Fizz-Bizz.js
+++ Fizz-Bizz.js
@@ -7,7 +7,7 @@ let buzz = 5
     }else if (i % 3 === 0){
         console.log("Fizz")
     }else if (i % 5 === 0){
-        console.log("Buzz")ls
+        console.log("Buzz")
     }else{
         console.log(i)
     }    
